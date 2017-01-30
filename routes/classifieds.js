b96db57d7f5c09376ded5baaa80cebafa92868fa
Devin Hurd <hurdaudio@gmail.com>
2017-01-29 17:54:53 -0700

    most routes pass tests.

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -38,82 +38,50 @@ router.get('/:id', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', (req, res, next) =>{
   knex('classifieds')
-  .insert({
-    title: req.body.title,
-    description: req.body.description,
-    price: req.body.price,
-    item_image: req.body.item_image
-    }, '*')
-  .then((data) => {
-    var obj = {
-      id: data[0].id,
-      title: data[0].title,
-      description: data[0].description,
-      price: data[0].price,
-      item_image: data[0].item_image
-    };
-    res.status(200).send(obj);
-  })
-  .catch((err) => {
-    next(err);
-  });
+     .insert({
+       title: req.body.title,
+       description: req.body.description,
+       price: req.body.price,
+       item_image: req.body.item_image
+     }, ['id', 'title', 'description', 'price', 'item_image'])
+     .then((results)=>{
+       res.send(results[0]);
+     })
+     .catch((err)=>{
+       next(err);
+     });
 });
 
-router.patch('/:id', (req, res, next) => {
+router.patch('/:id', (req, res, next) =>{
   knex('classifieds')
-    .update({
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      item_image: req.body.item_image
-    }, '*')
-    .where('id', req.params.id)
-    .then((result) => {
-      var obj = {
-        id: result[0].id,
-        title: result[0].title,
-        description: result[0].description,
-        price: result[0].price,
-        item_image: result[0].item_image
-      };
-      res.send(obj);
-    })
-    .catch((err) => {
-      next(err);
-    });
-  });
+     .where('id', req.params.id)
+     .update({
+       title: req.body.title,
+       description: req.body.description,
+       price: req.body.price,
+       item_image: req.body.item_image
+     }, ['id', 'title', 'description', 'price', 'item_image'])
+     .then((results)=>{
+       res.send(results[0]);
+     })
+     .catch((err)=>{
+       next(err);
+     });
+});
 
-  router.delete('/:id', (req, res, next) => {
-
-  knex('classifieds')
-    .where('id', req.params.id)
-    .first()
-    .then((row) => {
-      if (!row) {
-        return next();
-      }
-
-
-      return knex('classifieds')
-        .del()
-        .where('id', req.params.id);
-    })
-    .then((result) => {
-      var obj = {
-        id: result[0].id,
-        title: result[0].title,
-        description: result[0].description,
-        price: result[0].price,
-        item_image: result[0].item_image
-      };
-      delete result.id;
-      res.send((obj));
-    })
-    .catch((err) => {
-      next(err);
-    });
+router.delete('/:id', (req, res, next) =>{
+knex('classifieds')
+   .where('id', req.params.id)
+   .returning(['id', 'title', 'description', 'price', 'item_image'])
+   .del()
+   .then((results)=>{
+     res.send(results[0]);
+   })
+   .catch((err)=>{
+     next(err);
+   });
 });
 
 
