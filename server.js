95abeba811a75ff32fea4b41bc04c7e9fe65a3a7
Devin Hurd <hurdaudio@gmail.com>
2017-01-29 19:46:13 -0700

    massing all tests

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const messages = require('./routes/classifieds');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/classifieds',messages);
 
-const port = process.env.PORT || 3000;
+
+
 
 app.listen(port, () => {
   console.log('Listening on port', port);
